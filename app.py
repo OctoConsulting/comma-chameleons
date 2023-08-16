c146514616228f03f5c1729e2c4b86a501c9249e
deanchanter <37911234+deanchanter@users.noreply.github.com>
2023-08-16 06:17:05 -0400

    remove and stash old Prompt Engineering
    Old pipeline prompt for marketing can be used from prompt.py

--- app.py
+++ app.py
@@ -35,172 +35,26 @@ os.environ["OPENAI_API_KEY"] = get_secrets()  # Replace with your key
 ########### Prompt Engineering aka LangChain #######################
 #####Return LangChain Promt to bot for testing UI#####
 
-example = """Did you enjoy Leadership Awareness with Dr. Renee Carr? Join Renee as she leads you through a 3-week Leadership Competency cohort focused on Level 1 (Self). This will be the last offering for Level 1 (Self) in 2023.
+# Define the template for the conversation prompt
+template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
-Register now!
-
-Course and schedule information listed below.
-
-About the Leadership Competency Pillar
-The Leadership Competency Pillar Levels 1-3 are designed to equip agile leaders with advanced, practice-oriented skills for effectively leading teams in a Volatile, Uncertain, Complex, and Ambiguous (VUCA) world. Start with the Level 1 (Self) cohort to discover your personal alignment toward leadership. Upon completion of Level 1 (Self), you can immediately advance into Level 2 (Teams) this summer and Level 3 (Organizations) this fall. For each level, you’ll need 8 total hours across three weeks and earn 8 CLPs.
-
-Summer 2023
-July 11th — July 25th (Level 1 Self) *Last chance to take Level 1 (Self) in 2023!
-Tuesday, July 11th, 11:00 AM — 2:00 PM ET
-Tuesday, July 18th, 11:00 AM — 2:00 PM ET
-Tuesday, July 25th, 11:00 AM — 1:00 PM ET
-
-Register now!
-
-Interested in developing a foundational skill set in another learning track? View course and schedule information on Confluence.
-
-We hope you will join us,
-Workforce Resilience team
-
-Provide feedback on your learner experience by reaching out to Workforce Resilience at workforceresilience@cms.hhs.gov. Join us on Slack at #workforce_resilience_public.
-"""
-
-full_template = """
-    {introduction}
-    {start}"""
-#{example}
-
-
-full_prompt = PromptTemplate.from_template(full_template)
-
-introduction_template = """You write with the follow persona based on provided points.:
-
-1. Tone and Approach:
-   - Blend professionalism with an inviting and informative approach.
-   - Focus on delivering essential details while building anticipation and excitement in the reader.
-   - Prioritize clarity and conciseness to ensure easy understanding.
-   - Don't use Emojis.
-
-2. Establish Authority:
-   - Present factual information about the topic
-   - Highlight its purpose, key features, and relevant details.
-   - Create a sense of credibility by sharing authoritative information
-   - If you dont know say you need more info on the topic
-
-3. Engage Curiosity:
-   - Pose thought-provoking questions to spark the reader's interest.
-   - Offer insights into the value and benefits of the subject matter.
-   - Encourage readers to consider the impact of the topic on their interests or needs.
-
-4. Encourage Action:
-   - Use an enthusiastic tone to emphasize availability and time-sensitive aspects.
-   - Clearly state the call to action, such as registering, subscribing, or exploring further.
-   - Convey the urgency or importance of taking immediate steps.
-
-5. Alternative Options:
-   - Introduce alternative choices or options related to the main topic only if you know about them
-   - Provide brief but relevant information about each option if you know about them
-   - Maintain professionalism while conveying the advantages of each choice.
-
-6. Confidence and Excitement:
-   - Conclude with a positive note, leaving readers feeling informed and empowered.
-   - Express enthusiasm about the topic and its potential benefits.
-   - Encourage readers to embrace the topic with excitement and anticipation.
-
-7. Clarity and Brevity:
-   - Prioritize clear and concise language.
-   - Avoid overwhelming readers with excessive information.
-   - Present information in an organized and easy-to-follow manner.
-
-8. Personalization and Connection:
-   - Connect with readers by addressing their needs, interests, or pain points if you know about them otherwise default to the needs of a project manager
-   - Relate the topic to the readers' context whenever possible.
-
-9. Variation in Content:
-   - Apply the Informative Inviter persona to various contexts, such as event invitations, product launches, educational content, and more.
-   - Adapt the persona's features to suit the specific audience and goals of each writing.
-
-10. Review and Refinement:
-    - After drafting, review the content to ensure that it strikes the right balance between professionalism and excitement.
-    - Edit for clarity, coherence, and a smooth flow of information.
-
-11. DateTime
-    - When in a sentence Dates and Times are expressed like July 20th
-    - When listing multiples Dates and Times and time they are expressed like Thursday, July 20th, 9:30 AM - 10:00 AM ET
-
-"""
-
-introduction_prompt = PromptTemplate.from_template(introduction_template)
-
-example_template = """Here's an example: Seats are still available for the Product Management Awareness Bootcamp kicking off on July 20th.
-
-Register now!
-
-Course and schedule information listed below.
-
-About the Product Management Awareness Pillar
-
-Within the Awareness level, the Product Management Bootcamp cohort introduces you to product management and lays the groundwork for foundational concepts and practices you'll strengthen in the Competency Pillar. This cohort is designed as an adaptable option to allow learners to complete the cohort in a single session. Under the Awareness level, the Product learning track is designed to answer the questions of:
-
-· What is product management?
-
-· How does lean-agile inform how we approach product management?
-
-· What are the key roles and responsibilities of product management?
-
-· How are product management and the lean startup approach changing the ways we work in government - regardless of whether or not you are a product manager?
-
-Dates & times of the next cohort
-
-· July 20th - August 3rd
-
-Thursday, July 20th, 9:30 AM - 10:00 AM ET (Required Kickoff)
-
-Thursday, August 3rd, 9:00 AM - 12:30 PM ET (Bootcamp)
-
-Interested in a different track? These summer cohorts are kicking off soon!
-
-· Cloud
-
-July 12th - August 2nd
-
-Wednesday, July 12th, 11:00 AM - 11:30 AM ET (Kickoff)
-
-Wednesday, July 19th, 11:00 AM - 12:00 PM ET
-
-Wednesday, July 26th, 11:00 AM - 12:00 PM ET
-
-Wednesday, August 2nd, 11:00 AM - 12:00 PM ET
-
-· Cybersecurity August 3rd - August 24th Thursday, August 3rd, 10:00 AM - 10:30 AM ET (Kickoff) Thursday, August 10th, 10:00 AM - 11:00 AM ET Thursday, August 17th, 10:00 AM - 11:00 AM ET Thursday, August 24th, 10:00 AM - 11:00 AM ET
-
-· Human-Centered Design (HCD) Session 2 (NEW 1-Day Bootcamp): August 3rd Thursday, August 3rd, 9:00 AM - 1:00 PM ET Register now!
-
-For more information, visit the Workforce Resilience Confluence Space. Provide feedback on your learner experience by reaching out to Workforce Resilience at WorkforceResilience@cms.hhs.gov. Join us on Slack at #workforce_resilience_public."""
-#{example_c} """
-example_prompt = PromptTemplate.from_template(example_template)
-
-start_template = """
+Current conversation:
 {chat_history}
-Input: {input}
-Output:"""
-start_prompt = PromptTemplate.from_template(start_template)
+Human: {input}
+AI:"""
 
-input_prompts = [
-    ("introduction", introduction_prompt),
-#    ("example", example_prompt),
-    ("start", start_prompt)
-]
-pipeline_prompt = PipelinePromptTemplate(final_prompt=full_prompt, pipeline_prompts=input_prompts)
+# Create the prompt from the template
+prompt = PromptTemplate(
+    input_variables=["input", "chat_history"], template= template
+)
 
-#pipeline_prompt.input_variables
-def return_promt():
-    return pipeline_prompt.format(
-        example_c = example,
-        input="Progam Management Jan 18"
-    )
 
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
     llm = ChatOpenAI(temperature=.7, model='gpt-3.5-turbo-16k')
     memory = ConversationBufferMemory(memory_key='chat_history')
-    chain = LLMChain(llm=llm, prompt=pipeline_prompt, verbose=True, output_key='output', memory=memory)
+    chain = LLMChain(llm=llm, prompt=prompt, verbose=True, output_key='output', memory=memory)
     return chain
 
 
