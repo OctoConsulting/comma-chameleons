aea4a25770535e3c10d347740fb3f5fe57186344
John Harris <jwharris215@gmail.com>
2023-09-11 16:28:07 -0400

    Replace webhook method

--- app.py
+++ app.py
@@ -27,97 +27,35 @@ block.launch()#debug=True)
 
 
 
+authenticator = IAMAuthenticator('your-api-key')
+assistant = AssistantV2(
+    version='your-version-date',
+    authenticator=authenticator
+)
+assistant.set_service_url('your-service-url')
 
-# frontend = embedded chat ui in your HTML
-# backend = the webhook endpoint running the llms
-# 1. The client will embed Watson Assistant Widget in their HTML
-# 2. We will configure our Watson Assistants to trigger a webhook 
-# 3. Backend API (the python here) in the cloud will listen for webhook
-# 4. Respond payload back to frontend: Setup Watson Assistant to use the webhook response in dialog nodes in IBM Assistant Dashboard 
-# @app.route('/webhook', methods=['POST'])
-def webhook(request_data):
-    context = request_data.get('context', {}) # Assistant comes with/sends a 'context' variable
-    user_input = request_data.get('input', {}).get('text', '')
-    
-    # Append the latest user input to chat history
-    chat_history = context.get('chat_history', [])
-    chat_history.append({'user': user_input}) # send to a db?
+def webhook():
+    payload = request.json
+    text_input = payload['input']['text']
+    session_id = payload['session_id']
 
-    # Call your LLM/Langchain here and get the response, etc.
-    llm_response = chain.run(user_input, context)
-    chat_history.append({'llm': llm_response})
+    response = assistant.message(
+        assistant_id='your-assistant-id',
+        session_id=session_id,
+        input={
+            'message_type': 'text',
+            'text': text_input
+        }
+    ).get_result()
 
-    # Save/update context/history here (optional)
-    # this is where can customize context and interface with llm. on the Assistant Dashboard we can supposedly access this later and do node things in response to the 'context' values
-    # From docs: "In addition to maintaining our place in the conversation, the context can also contain action variables that store any other data you want to pass back and forth between your application and the assistant. This can include persistent data you want to maintain throughout the conversation (such as a customer's name or account number), or any other data you want to track (such as the contents of a shopping cart or user preferences)."
-    # context['some_new_variable'] = 'some_value'
-    context['chat_history'] = chat_history
+    session_history = assistant.get_session(
+        assistant_id='your-assistant-id',
+        session_id=session_id
+    ).get_result()
+
+    # You can pass response and session history to LLM things here.
 
     return jsonify({
-        'context': context,
-        'output': {
-            'generic': [
-                {
-                    'response_type': 'text',
-                    'text': llm_response
-                }
-            ]
-        },
+        'response': response,
+        'session_history': session_history
     })
-
-
-
-################ example of Python SDK style from IBM docs (does not go with our Assistant chatbox in same ways but shows another example of context maybe for later) #############
-# https://cloud.ibm.com/docs/watson-assistant?topic=watson-assistant-api-client&code=python
-
-# Example 3: Preserves context to maintain state.
-# from ibm_watson import AssistantV2
-# from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
-# # Create Assistant service object.
-# authenticator = IAMAuthenticator('{apikey}') # replace with API key
-# assistant = AssistantV2(
-#     version = '2021-11-27',
-#     authenticator = authenticator
-# )
-# assistant.set_service_url('{url}') # replace with service instance URL
-# assistant_id = '{environment_id}' # replace with environment ID
-
-# # Initialize with empty message to start the conversation.
-# message_input = {
-#     'message_type:': 'text',
-#     'text': ''
-#     }
-# context = {}
-
-# # Initialize with empty message to start the conversation.
-# message_input = {
-#     'message_type:': 'text',
-#     'text': ''
-#     }
-# context = {}
-
-# # Main input/output loop
-# while message_input['text'] != 'quit':
-
-#     # Send message to assistant.
-#     result = assistant.message_stateless(
-#         assistant_id,
-#         input = message_input,
-#         context = context
-#     ).get_result()
-
-#     context = result['context']
-
-#     # Print responses from actions, if any. Supports only text responses.
-#     if result['output']['generic']:
-#         for response in result['output']['generic']:
-#             if response['response_type'] == 'text':
-#                 print(response['text'])
-
-#     # Prompt for the next round of input unless skip_user_input is True.
-#     if not result['context']['global']['system'].get('skip_user_input', False):
-#         user_input = input('>> ')
-#         message_input = {
-#             'text': user_input
-#         }
