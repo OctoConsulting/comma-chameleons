fa4d1176bc52a4b19fb3ef78fa5d90e2aa50f456
John Harris <jwharris215@gmail.com>
2023-09-11 17:13:20 -0400

    Reset webhook

--- app.py
+++ app.py
@@ -23,39 +23,3 @@ block.launch()#debug=True)
 
 # if __name__ == "__main__":
 #     main()
-
-
-
-
-authenticator = IAMAuthenticator('your-api-key')
-assistant = AssistantV2(
-    version='your-version-date',
-    authenticator=authenticator
-)
-assistant.set_service_url('your-service-url')
-
-def webhook():
-    payload = request.json
-    text_input = payload['input']['text']
-    session_id = payload['session_id']
-
-    response = assistant.message(
-        assistant_id='your-assistant-id',
-        session_id=session_id,
-        input={
-            'message_type': 'text',
-            'text': text_input
-        }
-    ).get_result()
-
-    session_history = assistant.get_session(
-        assistant_id='your-assistant-id',
-        session_id=session_id
-    ).get_result()
-
-    # You can pass response and session history to LLM things here.
-
-    return jsonify({
-        'response': response,
-        'session_history': session_history
-    })
