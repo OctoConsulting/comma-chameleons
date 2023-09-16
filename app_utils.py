785c09f751c3692ffb12d232959c5c74a5b3e5ec
John Harris <jwharris215@gmail.com>
2023-09-16 13:08:08 -0400

    Refactor Gradio 2nd

--- app_utils.py
+++ app_utils.py
@@ -1,68 +1,78 @@
-# Gradio UI 1
+# Gradio UI 1: "Customization and file uploads", "Gradio blocks style"
 # https://www.gradio.app/demos
 # https://python.langchain.com/docs/integrations/tools/gradio_tools
 import gradio as gr
 
+# Read and return content from a file given its path.
 def read_file_from_path(file_path):
     with open(file_path, 'r') as f:
         content = f.read()
     return content
 
+# Process an uploaded file and return its content. Used to allow user to upload local files through Gradio.
 def process_uploaded_file(file):
     file_path = file.name        
     content = read_file_from_path(file_path)
     return content
 
+# Update the chat history and display a message based on the provided input.
+# This method is example of how to play with how the chat history is 
+# displayed on the client side. 
 def display_text(message, chat_history):
     chat_history.clear()  # Clear the existing chat history
-
+    
+    # Provide content based on the specific message received.
     if message == 'How to Amend an Approved Permit, Registration, or Notice using Permits Online':
-        content = '<span id="tab-one-content">Select the application that you would like to amend: If you need to update information about the business entity ONLY, choose "Create Amendment" for the Application for Original Entity. If you need to update information about a particular commodity operation, choose "Create Amendment" for that operation (e.g. Application for Winery Operations). If you need to update both a commodity operation and the associated business entity information, choose "Create Amendment" for the commodity operation record.  You will be given the opportunity to update both the commodity and entity information as part of this process. </span>'
+        content = '<span id="tab-one-content">Select the application that you would like to amend: ...</span>'
     else:
         content = '<span id="tab-two-content">What permit are you applying for?</span>'
+    
     bot_message = content
     chat_history.append((None, bot_message))
     return "", chat_history
 
+# Return information based on the type of permit or registration requested.
+# This method is example of how to configure Gradio 'examples' whos output differs from
+# their displayed message. 
 def display_message(button_text):
+    # Create detailed messages for each type of action a user can take.
     if button_text == 'Apply for and update the permit, registration, or notice.':
-        return '**Press send to ask the ai more about this** Permits Online makes it easy for you to apply for and update the permit, registration, or notice you need to operate a TTB-regulated business. Once you have registered to use Permits Online, just follow the instructions and prompts for submitting your application package.  There is no fee to apply. Permits Online makes it easy for you to apply for and update the permit, registration, or notice you need to operate a TTB-regulated business.  Once you have registered to use Permits Online, just follow the instructions and prompts for submitting your application package.  There is no fee to apply.'
+        return '**Press send to ask the ai more about this** Permits Online makes it easy for you ...'
     elif button_text == 'Apply for a Certificate of Label Approval/Exemption (COLA).':
-        return '**Press send to ask the ai more about this** COLAs Online makes it easy for you to apply for a Certificate of Label Approval/Exemption (COLA) for your product. Once you have registered, just follow the instructions and prompts for submitting your online application. In most cases, once your application is submitted electronically, you\'re done. There are no forms to sign and... there is no fee to apply. COLAs Online makes it easy for you to apply for a Certificate of Label Approval/Exemption (COLA) for your product. Once you have registered, just follow the instructions and prompts for submitting your online application. In most cases, once your application is submitted electronically, you\'re done. There are no forms to sign and... there is no fee to apply.'
+        return '**Press send to ask the ai more about this** COLAs Online makes it easy for you ...'
     elif button_text == 'Your wine, distilled spirit, or beer/malt beverage may require formula approval or laboratory sample analysis':
-        return '**Press send to ask the ai more about this** Your wine, distilled spirit, or beer/malt beverage may require formula approval or laboratory sample analysis before you may apply for a Certificate of Label Approval, or COLA. We require formula approval most commonly when a product has added flavoring or coloring materials. New to Formula Approval? A formula is a complete list of the ingredients used to make an alcohol beverage and a step-by-step description of how it\'s made. In some cases, we may also require laboratory analysis of the product. Your wine, distilled spirit, or beer/malt beverage may require formula approval or laboratory sample analysis before you may apply for a Certificate of Label Approval, or COLA. We require formula approval most commonly when a product has added flavoring or coloring materials. New to Formula Approval? A formula is a complete list of the ingredients used to make an alcohol beverage and a step-by-step description of how it\'s made. In some cases, we may also require laboratory analysis of the product. Your wine, distilled spirit, or beer/malt beverage may require formula approval or laboratory sample analysis before you may apply for a Certificate of Label Approval, or COLA. We require formula approval most commonly when a product has added flavoring or coloring materials. New to Formula Approval? A formula is a complete list of the ingredients used to make an alcohol beverage and a step-by-step description of how it\'s made. In some cases, we may also require laboratory analysis of the product. Your wine, distilled spirit, or beer/malt beverage may require formula approval or laboratory sample analysis before you may apply for a Certificate of Label Approval, or COLA. We require formula approval most commonly when a product has added flavoring or coloring materials. New to Formula Approval? A formula is a complete list of the ingredients used to make an alcohol beverage and a step-by-step description of how it\'s made. In some cases, we may also require laboratory analysis of the product.'
+        return '**Press send to ask the ai more about this** Your wine, distilled spirit, or beer/malt beverage may require ...'
 
-# def predict(inp, history):
-#     #output = chain.run({'example_c':example,'input':inp})
-#     output = chain.run({'input':inp})
-#     history.append((inp, output))
-#     return "", history
-
+# Make a call to LLM API that sends Gradio input and history as payload, update the history accordingly.
 def predict(inp, history):
     print("this is the input:" + inp)
     print(history)
+    
+    # Authentication for IBM Cloud API
     import requests
     API_KEY = '5Oz7Ma1ZCctfI0I9FU1kbZC6YRWm9jRKddtL-8mJYREx'
     token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
     mltoken = token_response.json()["access_token"]
-
+    
+    # Set up headers for the API request
     header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
-
-    # payload_scoring = {"input_data": [{"fields": [array_of_input_fields], "values": [array_of_values_to_be_scored, another_array_of_values_to_be_scored]}]}
+    
+    # Prepare and send the API request for scoring (prediction)
     payload_scoring = {"input_data": [{"values": [history + [inp]]}]}
     response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/ce451240-81cf-41ac-856d-a00153c75bbd/predictions?version=2021-05-01', 
                        json=payload_scoring, 
                        headers={'Authorization': 'Bearer ' + mltoken})
-
+    
+    # Parse and retrieve the response
     response_json = response_scoring.json()
     print(response_json)
-
+    
+    # Get the predicted output and update the history
     output = response_json.get('predictions', [{}])[0].get('values', [[""]])[0][0]
-
     history.append((inp, output))
     return "", history
 
-################## custom css #########################
+# Custom css for the Gradio. See Gradio docs for how the classes and ids were implemented. 
 css = """
 #warning {background-color: #FFCCCB} 
 .feedback textarea {font-size: 24px !important}
@@ -112,16 +122,17 @@ div > button.related-links-bottom {
 #examples-container {flex-grow: 0 !important;}
 """
 
-def initialize_ui():#chain: LLMChain ,prompt: PromptTemplate):
+# Gradio blocks style UI
+def initialize_ui():  # Initialize the Gradio UI
     """Initialize and launch the Gradio UI."""
     with gr.Blocks(css=css) as block:
-
+        # Displaying header text
         gr.Markdown("<h3><center>TTB.gov watsonx Demo</center></h3>", elem_id="header-subtext")
 
         with gr.Row():
 
             with gr.Column():
-
+                # Input textbox for user queries
                 with gr.Row():
                     message = gr.Textbox(
                         label="Input",
@@ -129,14 +140,17 @@ def initialize_ui():#chain: LLMChain ,prompt: PromptTemplate):
                         lines=8,
                         elem_id="chat-prompt"
                     ).style(container=False)
-                    
+
+                    # Button to upload a file
                     with gr.Row(elem_id="chat-prompt-row"):
                         with gr.Column(scale=0.09, min_width=0):
-                            upload_btn = gr.UploadButton("📁 File", file_types=["", ".", ".csv",".xls",".xlsx", "text"],  file_count="single", elem_id="upload-btn")
+                            upload_btn = gr.UploadButton("📁 File", file_types=["", ".", ".csv", ".xls", ".xlsx", "text"], file_count="single", elem_id="upload-btn")
 
-                with gr.Column(scale=0.09, min_width=0):  
+                # Button to submit the query
+                with gr.Column(scale=0.09, min_width=0):
                     submit = gr.Button(value="➦", variant="secondary").style(full_width=False)
-                
+
+                # Buttons for example queries
                 with gr.Column(elem_id="examples-container"):
                     gr.Markdown("<h3><center>≣ Try Asking</center></h3>", elem_id="related-header-subtext")
 
@@ -148,47 +162,47 @@ def initialize_ui():#chain: LLMChain ,prompt: PromptTemplate):
                         with gr.Column(scale=0.2, min_width=0):
                             example3_button = gr.Button("Your wine, distilled spirit, or beer/malt beverage may require formula approval or laboratory sample analysis", elem_classes="tab-button")
 
-                # with gr.Row():
-                #     # file_output = gr.File()
-
-                #     with gr.Accordion("Available Files", open=False):
-                #         file_display = gr.Textbox(label="File Contents", lines=10, readonly=True) 
-
-                # gr.Examples(
-                #     examples=[
-                #         "Hi! How's it going?",
-                #         "What should I do tonight?",
-                #         "Whats 2 + 2?",
-                #     ],
-                #     inputs=message,
-                # )
-
-                # gr.HTML("Demo application of a LangChain chain.")
-
+            # Chatbot display section
             with gr.Column(elem_id="chatbot-container"):
-
                 chatbot = gr.Chatbot(height=700, label="watsonx", show_copy_button=True)
 
+                # Additional buttons for quick links or popular searches
                 with gr.Column(elem_id="related-links-container"):
                     gr.Markdown("<h3><center>🌩 Quick Search Popular</center></h3>", elem_id="related-header-subtext")
                     tab1_button = gr.Button("How to Amend an Approved Permit, Registration, or Notice using Permits Online", elem_classes="related-links")
                     tab2_button = gr.Button("What to Gather Before you Apply", elem_classes="related-links-bottom")
 
+        # Binding actions to buttons
         tab1_button.click(display_text, inputs=[tab1_button, chatbot], outputs=[chatbot, chatbot])
         tab2_button.click(display_text, inputs=[tab2_button, chatbot], outputs=[chatbot, chatbot])
-
         example1_button.click(display_message, inputs=[example1_button], outputs=[message])
         example2_button.click(display_message, inputs=[example2_button], outputs=[message])
         example3_button.click(display_message, inputs=[example3_button], outputs=[message])
-
         submit.click(predict, inputs=[message, chatbot], outputs=[message, chatbot])
-        message.submit(predict, inputs=[message, chatbot], outputs=[message, chatbot]) #, [txt, state], [chatbot, state]
 
-        # .upload is an abstraction of an event listener when the user clicks the upload_btn
+        # Upload button event handling. .upload is an abstraction of an event listener when the user clicks the upload_btn.
         upload_btn.upload(process_uploaded_file, inputs=upload_btn, outputs=message)
+        
+        # Examples of basic Gradio accordions and chatbot 'examples'
+        # with gr.Row():
+        #     # file_output = gr.File()
 
+        #     with gr.Accordion("Available Files", open=False):
+        #         file_display = gr.Textbox(label="File Contents", lines=10, readonly=True) 
+
+        #     gr.Examples(
+        #         examples=[
+        #             "Hi! How's it going?",
+        #             "What should I do tonight?",
+        #             "Whats 2 + 2?",
+        #         ],
+        #         inputs=message,
+        #     )
+
+        # Examples of Gradio HTML components
         # gr.HTML(
-        #     "<center>Powered by <a href='https://github.com/hwchase17/langchain'>LangChain 🦜️🔗</a></center>"
+        #     "<center>Powered by <a href='https://www.ibm.com/docs/en/watsonx-as-a-service?topic=overview-watsonx'>watsonx 🦜️🔗</a></center>"
         # 
-    return block
-#block.launch()#debug=True)
+
+    return block  # Return the UI block
+    #block.launch()#debug=True) # Return the UI block when it is outside of a method
