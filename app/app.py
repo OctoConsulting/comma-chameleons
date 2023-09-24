30394728d4dcf684a08e8ed5c25e522bbeb6fc3f
deanchanter <37911234+deanchanter@users.noreply.github.com>
2023-09-24 08:46:44 -0400

    cleanup main
    Not tested!

---
+++ app/app.py
@@ -0,0 +1,26 @@
+# Local Imports
+from llm_utils import load_data_from_huggingface, load_vector_db, RAGUtils, Credentials
+from app_utils import initialize_ui
+
+# Initialize the RAG pipeline 
+def initialize_rag(db, creds):
+    return RAGUtils(db, creds)
+
+# Launch the Gradio UI
+def launch_gradio(predict):
+    block = initialize_ui(predict)
+    block.launch()
+
+# Initialize the RAG pipeline and also the Launch the Gradio UI
+def main():
+    ###### Build Vector DB ########
+    db = load_vector_db("./chroma_db_wiki_base")
+
+    ###### Langchain ########
+    creds = Credentials('key.ini')
+    rag = initialize_rag(db,creds)
+    predict = rag
+    launch_gradio(predict)
+
+if __name__ == "__main__":
+    main()
